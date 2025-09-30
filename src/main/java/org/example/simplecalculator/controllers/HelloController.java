ffc57e127403ba6d82862ddffdf3630b0ff94aeb
murik <dennyisgodxd@gmail.com>
2025-09-30 10:02:29 +0200

    initial commit

---
+++ src/main/java/org/example/simplecalculator/controllers/HelloController.java
@@ -0,0 +1,30 @@
+package org.example.simplecalculator.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+
+    // operators
+    @FXML
+    private void onBtnAdditionClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void onBtnSubtractClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void onBtnMultiplyClick(ActionEvent actionEvent) {
+    }
+
+    @FXML
+    private void onBtnDivisionClick(ActionEvent actionEvent) {
+    }
+
+    // calculate
+    @FXML
+    private void onBtnCalculateClick(ActionEvent actionEvent) {
+    }
+}
